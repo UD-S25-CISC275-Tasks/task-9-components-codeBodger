a68f9f733cf65e5d9546509eb4aea761ae4d8e52
codeBodger <diesel.ack+github@gmail.com>
2025-02-24 21:40:14 -0500

    Wrote ChangeType?

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -2,6 +2,28 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
-export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+export function ChangeType(): /* React. */ JSX.Element {
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    return (
+        <div>
+            <h2>Change Type</h2>
+            <Button
+                onClick={() =>
+                    setType(
+                        type === "multiple_choice_question"
+                            ? "short_answer_question"
+                            : "multiple_choice_question"
+                    )
+                }
+            >
+                Change Type
+            </Button>
+            <h2>
+                {type === "multiple_choice_question"
+                    ? "Multiple Choice"
+                    : "Short Answer"}
+            </h2>
+        </div>
+    );
 }
