da94eef1ef3e0acc6c9b15d0d690ed2285a4714a
codeBodger <diesel.ack+github@gmail.com>
2025-02-24 21:31:55 -0500

    Wrote TwoDice?

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -11,6 +11,18 @@ export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
-export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+export function TwoDice(): /* React. */ JSX.Element {
+    const [lDie, setLDie] = useState<number>(2);
+    const [rDie, setRDie] = useState<number>(4);
+
+    return (
+        <div>
+            <h2>Two Dice</h2>
+            <span data-testid="left-die">{lDie}</span>
+            <span data-testid="right-die">{rDie}</span>
+            <Button onClick={() => setLDie(d6())}>Roll Left</Button>
+            <Button onClick={() => setRDie(d6())}>Roll Right</Button>
+            <h2>{lDie === rDie ? (lDie === 1 ? "Lose" : "Win") : ""}</h2>
+        </div>
+    );
 }
