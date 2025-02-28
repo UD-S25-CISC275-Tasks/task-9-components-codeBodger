656b384b0e144bf8d9568d02ff6e24aea2562782
codeBodger <diesel.ack+github@gmail.com>
2025-02-27 23:21:25 -0500

    Fixed Gradescope's errors that aren't my problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 // You will not need to modify this file
 
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
+
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
